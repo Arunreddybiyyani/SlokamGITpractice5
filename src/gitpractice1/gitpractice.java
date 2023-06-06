eee5fca0bcee450884ec2c97f00a19d699924630
BIYYANI ARUN KUMAR REDDY <arunreddy453@gmail.com>
2023-06-06 20:29:30 +0530

    practic git eclipse

---
+++ src/gitpractice1/gitpractice.java
@@ -0,0 +1,12 @@
+package gitpractice1;
+
+public class gitpractice {
+	
+	public static void main(String[] args) {
+		
+		int a = 7896;
+		double b = 78963.3;
+		double c = a*b;
+		System.out.println(c);
+	}
+}
